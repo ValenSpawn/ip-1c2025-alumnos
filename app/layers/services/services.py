82d613bca040926799603f44e7725e98bd252155
ValenSpawn <valengalvan8@gmail.com>
2025-06-28 16:05:35 -0300

    modificacion de la funcion favoritos

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -44,20 +44,16 @@ def saveFavourite(request):
     return repositories.save_favourite(fav) # lo guardamos en la BD.
 
 # usados desde el template 'favourites.html'
-def getAllFavourites(request):
-    if not request.user.is_authenticated:
+def getAllFavouritesByUser(user):
+    if not user.is_authenticated:
         return []
-    else:
-        user = get_user(request)
 
-        favourite_list = [] # buscamos desde el repositories.py TODOS Los favoritos del usuario (variable 'user').
-        mapped_favourites = []
-
-        for favourite in favourite_list:
-            card = '' # convertimos cada favorito en una Card, y lo almacenamos en el listado de mapped_favourites que luego se retorna.
-            mapped_favourites.append(card)
-
-        return mapped_favourites
+    favourite_list = repositories.get_favourites_by_user(user) 
+    mapped_favourites = [
+        translator.fromModelIntoCard(fav) for fav in favourite_list
+    ]
+    return mapped_favourites
+    
 
 def deleteFavourite(request):
     favId = request.POST.get('id')
